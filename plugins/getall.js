7ac6f1ddcf774661e81a77db64b53aa5cdab334b
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-12-10 21:58:33 +0500

    getall

---
+++ plugins/getall.js
@@ -0,0 +1,94 @@
+/**
+
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                      //
+//                                ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                                   //
+//                                                                                                      // 
+//                                         Ｖ：1．2．2                                                   // 
+//                                                                                                      // 
+//            ███████╗██╗   ██╗██╗  ██╗ █████╗ ██╗██╗         ███╗   ███╗██████╗                        //
+//            ██╔════╝██║   ██║██║  ██║██╔══██╗██║██║         ████╗ ████║██╔══██╗                       //
+//            ███████╗██║   ██║███████║███████║██║██║         ██╔████╔██║██║  ██║                       //
+//            ╚════██║██║   ██║██╔══██║██╔══██║██║██║         ██║╚██╔╝██║██║  ██║                       //
+//            ███████║╚██████╔╝██║  ██║██║  ██║██║███████╗    ██║ ╚═╝ ██║██████╔╝                       //
+//            ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝    ╚═╝     ╚═╝╚═════╝                        //
+//                                                                                                      //
+//                                                                                                      //
+//                                                                                                      //
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+
+CURRENTLY RUNNING ON BETA VERSION!!
+*
+   * @project_name : Suhail-Md
+   * @author : Suhail <https://github.com/SuhailTechInfo>
+   * @youtube : https://www.youtube.com/c/@SuhailTechInfo0
+   * @infoription : Suhail-Md ,A Multi-functional whatsapp user bot.
+   * @version 1.2.5 
+*
+   * Licensed under the  GPL-3.0 License;
+* 
+   * ┌┤Created By Suhail Tech Info.
+   * © 2023 Suhail-Md ✭ ⛥.
+   * plugin date : 10/12/2023
+* 
+   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   * SOFTWARE.
+**/
+
+
+
+
+
+
+
+const {
+    smd,
+    tlang,
+    prefix, 
+    Config ,
+    sleep,
+    getBuffer, 
+    smdJson,
+    smdBuffer
+     } = require('../lib')
+
+
+
+
+
+smd({
+    cmdname: "getall",
+    desc: "get jid of all members of groups/pm chats/all groups.",
+    type: "owner",
+    fromMe:true,
+    use:"[ members / user / groups ]",
+    usage:"get jids of groups,personal chats, also members of group, so that used them for forward cmd!",
+    filename: __filename,
+    public : false,
+},
+async (citel, text, { store }) => {
+  try{
+let str = "";
+    let cd = text.split(" ")[0]
+    if(cd === "members" || cd === "member") {
+      if (!citel.isGroup) return citel.reply(tlang("group"));
+      const participants = citel.metadata.participants || {};
+      for (let i of participants) {    str += `📍 ${i.id}\n`;   }
+      str ? citel.reply(`*「 LIST OF GROUP MEMBER'S JID 」*\n\n` +str) : citel.reply("*Request Denied!*")
+    }else if(cd == "user" || cd == "pm" || cd == "pc"){
+        let anu = await store.chats.all().filter(v => v.id.endsWith('.net')).map(v => v)
+        for (let i of anu) { str += `📍 ${i.id}\n` }
+        str ? citel.reply(`*「 LIST OF PERSONAL CHAT JIDS 」*\n\nTotal ${anu.length} users are text in personal chat.\n\n` + str) : citel.reply("*Request Denied!*")
+    }else if(cd == "group" || cd == "groups" || cd == "gc"){
+      n = await citel.bot.groupFetchAllParticipating();
+      const c=Object.entries(n).slice(0).map(t=>t[1]);
+      for(var i of c.map(t=>t.id)){  str += `📍 ${i}\n`;  } 
+      str ? citel.reply(`*「 LIST OF GROUP CHAT JIDS」*\n\n` + str) : citel.reply("*Request Denied!*")
+  }else return await citel.reply(`*Use ${prefix}getall pc| gc| member!*`)
+}catch(e){ citel.error(`${e}\n\nCommand getall`,e)}
+});
