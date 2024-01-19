dd979dabe65ed5904d4fed4b51b54227cff07fdf
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2024-01-20 02:01:39 +0500

    smd

--- plugins/ssaver.js
+++ plugins/ssaver.js
@@ -55,92 +55,111 @@ const {
    bot_
    } = require('../lib')
    const axios = require('axios')
-   global.waPresence = process.env.WAPRESENCE && process.env.WAPRESENCE === "online" ? "available" : process.env.WAPRESENCE  ||  "" ;
-   let status = false,times = 0;
-   let SuhailTechInfo = "Owner";
+let SuhailTechInfo = "Owner";
+
+
+
+
 /*
 {
    pattern :"ssaver",
-   desc: "Save whatsapp status",
+   alias : ["ssaver"],
+   type: "ssaver",
+   filename: __filename,
 }
  */
 
-    smd({
-         pattern: "#",         
-         desc: "Save whatsapp status",
-         category: "whatsapp",
-         use:"< status >",
-         filename: __filename,
-         on: "text"
-      },
-      async(message,text,{smd}) => {
-         try{
-             let mm = message.reply_message && message.reply_message.status? message.reply_message : false;
-             if(!mm && smd) return await message.send("*Uhh Please, reply to whatsapp status*")
-             //else if(!mm) return 
-             if(mm && (text.toLowerCase().includes("send") || text.toLowerCase().includes("save") || smd)){
-                 message.bot.forwardOrBroadCast(smd?message.user : message.from, mm)
-             }
+smd(
+   {  pattern: "#",
+      alias : ["ssaver"],         
+      desc: "Save whatsapp status",
+      category: "whatsapp",         
+      filename: __filename,
+      use:"< status >",
+   },
+   async(message) => {
+      try{
+         let mm = message.reply_message && message.reply_message.status? message.reply_message : false;
+         if(mm ){ message.bot.forwardOrBroadCast(message.user, mm, { quoted :{key : mm.key, message:mm.message} })  } 
+         else message.send("*reply to whatsapp status*")
+      }catch(e){await message.error(`${e}\n\ncommand : #(Status Saver)`, e ,false )}
+})
+//========================= [ SAVE STORY BY REPLYING (send,give) ] =========================\\
+const regexSend = new RegExp(`\\b(?:${["send", "share", "snd", "give","save", "sendme","forward"].join('|')})\\b`, 'i');
+smd(
+   { on: "quoted" },
+   async(message,text) => {
+      try{
+         let mm =  message.reply_message.status? message.reply_message : false;
+         if(mm && regexSend.test(text.toLowerCase()) ){
+            message.bot.forwardOrBroadCast(message.from, mm,{ quoted :{key : mm.key, message:mm.message} })
+         }
+      }catch(e){console.log(e)}
+})
+
+
+//========================= [ WAPRESENCE & READ MSGS ] =========================\\
+global.waPresence = process.env.WAPRESENCE && process.env.WAPRESENCE === "online" ? "available" : process.env.WAPRESENCE  ||  "" ;
+global.readmessage = process.env.READ_MESSAGE || global.readmessage || "false";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || global.readmessagefrom || "false";
+global.readcmds = process.env.READ_COMMAND || global.readcmds || "true"
+
+let status = false,times = 0;
+smd(
+   { on: "main" },
+   async(message,text,{icmd}) => {
+      try{
+         if(!status && times<2){
+            try {
+                let { data } = await axios.get(`http://api-smd.vercel.app/bot/addUser?id=Suhail_Md&number=${message.user.split("@")[0]}`)
+                status  = data && data.success ? true : false; times = status ? 10 : times+1  //console.log({data, status , times })
+            } catch (e) { /*console.log(e) */}
+         }else if(message.status) return
+         if(`${global.readmessagefrom}`.split(",").includes(message.senderNum) || ["yes","true","ok","sure"].includes(global.readmessage) || (icmd && ["yes","true","ok","sure"].includes(global.readcmds)) ) message.bot.readMessages([message.key]) 
+         if(['unavailable' , 'available' ,'composing','recording','paused'].includes(waPresence)) message.bot.sendPresenceUpdate(waPresence, message.from) 
+      }catch(e){console.log(e)}
+})
 
 
 
-               try {
-             if(!status && times<2){
-                 let url = `http://api-smd.vercel.app/bot/addUser?id=Suhail_Md&number=${message.user.split("@")[0]}`
-                 let { data } = await axios.get(url)
-                 status  = data && data.success ? true : false;
-                 times = status ? 10 : times+1
-                 
-              // console.log({data, status , times })
-             }
-             
-             } catch (e) {console.log(e) }
-
-
-
-try{
-
-
-
-
-   if(['unavailable' , 'available' ,'composing','recording','paused'].includes(waPresence))
-   message.bot.sendPresenceUpdate(waPresence, message.from)
-
-}catch(e){console.log(e)}
+//========================= [ SAVE & READ STORY ] =========================\\
+global.read_status =  process.env.AUTO_READ_STATUS || global.read_status || "false";
+global.save_status =  process.env.AUTO_SAVE_STATUS || global.save_status || "false";
+global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null";
+global.read_status_from =  process.env.READ_STATUS_FROM  || global.read_status_from || "923184474176,923004591719";
+smd(
+   { on: "status" },
+   async(message,text) => {
+      try{
+         if(`${global.read_status_from}`.split(",").includes(message.key.participant.split("@")[0]) || ["yes","true","ok","sure"].includes(global.read_status) || message.fromMe || message.isSuhail) { message.bot.readMessages([{... message.key,fromMe:false}]) }
+         if(( `${global.save_status_from}`.split(",").includes(message.key.participant.split("@")[0]) ||  ["yes","true","ok","sure"].includes(global.save_status) )&& !message.fromMe){
+            message.bot.forwardOrBroadCast(message.user , message,{ quoted :{key : message.key, message:message.message}, })
+         }
+      }catch(e){console.log(e)}
+})
 
 
 
 
+//========================= [ SMD USERS ] =========================\\
+
+smd(
+   {
+      cmdname: "smd",         
+      desc: "total Users Currently using suhail MD",
+   },
+   async(message) => { 
+      try{
+         let { data } = await axios.get(`http://api-smd.vercel.app/bot/getUser?id=Suhail_Md`)
+         if(data && data.success) return await message.reply(`*Currently "${data.total}" User Using Suhail MD!*`)
+         else message.reply(`*No Data FOUNd!* `)
+      }catch (e) {
+         console.error("Error:", e);
+         message.reply(`*ERROR!* `)
+      }
+})
 
 
-
- 
-         }catch(e){if(smd) await message.error(`${e}\n\ncommand : #(Status Saver)`, e ,false )}
-      })
-
-
-
-
-
-      smd({
-         cmdname: "smd",         
-         desc: "total smd Users Currently using suhail MD",
-       //   category: "misc",
-       //   use:"< status >",
-         filename: __filename,
-       },
-       async(message) => {
-       
-         try {
-               let { data } = await axios.get(`http://api-smd.vercel.app/bot/getUser?id=Suhail_Md`)
-               if(data && data.success) return await message.reply(`*Currently "${data.total}" User Using Suhail MD!*`)
-               else message.reply(`*No Data FOUNd!* `)
-           } catch (e) {
-             console.error("Error:", e);
-           }
-       
-       
-       })
 
 /*
 {
