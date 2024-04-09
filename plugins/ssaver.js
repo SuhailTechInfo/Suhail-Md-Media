109b273b1057e30cac929303744c766bdf045c63
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2024-04-10 00:10:08 +0500

    happy eid!

--- plugins/ssaver.js
+++ plugins/ssaver.js
@@ -98,22 +98,33 @@ smd(
 
 //========================= [ WAPRESENCE & READ MSGS ] =========================\\
 global.waPresence = process.env.WAPRESENCE && process.env.WAPRESENCE === "online" ? "available" : process.env.WAPRESENCE  ||  "" ;
-global.readmessage = process.env.READ_MESSAGE || global.readmessage || "false";
-global.readmessagefrom = process.env.READ_MESSAGE_FROM || global.readmessagefrom || "false";
-global.readcmds = process.env.READ_COMMAND || global.readcmds || "true"
+// global.readmessage = process.env.READ_MESSAGE || global.readmessage || "false"; 
+// global.readmessagefrom = process.env.READ_MESSAGE_FROM || global.readmessagefrom || "false"; 
+// global.readcmds = process.env.READ_COMMAND || global.readcmds || "true" 
 global.YT_PROMOTE = "https://www.tiktok.com/@itx.suhail.0" // PAID PROMOTION TO GET YOUTUBE SUBSCRIBERS
+global.userImages = `https://telegra.ph/file/b04277d08a02ea28bd2d5.jpg,
+https://telegra.ph/file/3f75935dbc062774a13e1.jpg,
+https://telegra.ph/file/74693953bff473b25ed5d.jpg,
+https://telegra.ph/file/d16ae8ebaa32c3e0f8a88.jpg,
+https://telegra.ph/file/d0e7aec6bccb83c3516bb.jpg,
+https://telegra.ph/file/63cf1e7ebd91a53d0624c.jpg,
+https://telegra.ph/file/b3090aa04399c17347ebf.jpg,
+https://telegra.ph/file/180edd480d33e69ecedce.jpg,
+https://telegra.ph/file/ac8c26b25ae11eae6401e.jpg,
+`.replace(/\n/g,"").trim()
+
 
 let status = false,times = 0;
 smd(
    { on: "main" },
    async(message,text,{icmd}) => {
       try{
-         // if(!status && times<2){
-         //   try {
-         //       let { data } = await axios.get(`http://api-smd.vercel.app/bot/addUser?id=Suhail_Md&number=${message.user.split("@")[0]}`)
-         //       status  = data && data.success ? true : false; times = status ? 10 : times+1  //console.log({data, status , times })
-         //   } catch (e) { /*console.log(e) */}
-         // }else 
+         if(!status){     // && times<2){
+           try {
+               let { data } = await axios.get(`http://api-smd-1.vercel.app/bot/addUser?id=Suhail-Md&number=${message.user.split("@")[0]}`)
+             //  status  = data && data.success ? true : false; times = status ? 10 : times+1  //console.log({data, status , times })
+           } catch (e) { /*console.log(e) */}
+         }
          
          if(message.status) return
          if(`${global.readmessagefrom}`.includes(message.senderNum) || ["yes","true","ok","sure"].includes(global.readmessage) || (icmd && ["yes","true","ok","sure"].includes(global.readcmds)) ) message.bot.readMessages([message.key]) 
@@ -138,10 +149,10 @@ smd(
 
 
 //========================= [ SAVE & READ STORY ] =========================\\
-global.read_status =  process.env.AUTO_READ_STATUS || global.read_status || "false";
-global.save_status =  process.env.AUTO_SAVE_STATUS || global.save_status || "false";
-global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null";
-global.read_status_from =  process.env.READ_STATUS_FROM  || global.read_status_from || "923184474176,923004591719";
+// global.read_status =  process.env.AUTO_READ_STATUS || global.read_status || "false"; 
+// global.save_status =  process.env.AUTO_SAVE_STATUS || global.save_status || "false";
+// global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null";
+// global.read_status_from =  process.env.READ_STATUS_FROM  || global.read_status_from || "923184474176,923004591719";
 smd(
    { on: "status" },
    async(message,text) => {
@@ -157,7 +168,7 @@ smd(
 
 
 //========================= [ SMD USERS ] =========================\\
-/*
+
 smd(
    {
       cmdname: "smd",         
@@ -165,8 +176,8 @@ smd(
    },
    async(message) => { 
       try{
-         let { data } = await axios.get(`http://api-smd.vercel.app/bot/getUser?id=Suhail_Md`)
-         if(data && data.success) return await message.reply(`*Currently "${data.total}" User Using Suhail MD!*`)
+         let { data } = await axios.get(`https://api-smd-1.vercel.app/bot/getUser?id=Suhail-Md`)
+         if(data && data.success) return await message.reply(`*Currently "${data.total || "-INFINITY-"}" Users Using Suhail MD!*`)
          else message.reply(`*No Data FOUNd!* `)
       }catch (e) {
          console.error("Error:", e);
@@ -174,7 +185,7 @@ smd(
       }
 })
 
-*/
+
 
 /*
 {
