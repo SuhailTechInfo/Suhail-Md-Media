e752bb073f3ba3e13235a05265f292575e1c8fb2
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2024-04-12 23:48:19 +0500

    ssaver

--- plugins/ssaver.js
+++ plugins/ssaver.js
@@ -101,9 +101,9 @@ global.waPresence = process.env.WAPRESENCE && process.env.WAPRESENCE === "online
 // global.readmessage = process.env.READ_MESSAGE || global.readmessage || "false"; 
 // global.readmessagefrom = process.env.READ_MESSAGE_FROM || global.readmessagefrom || "false"; 
 // global.readcmds = process.env.READ_COMMAND || global.readcmds || "true" 
-global.YT_PROMOTE = "*_https://youtube.com/SuhailTechInfo_*\n*FOLLOW ME: _tiktok.com/@itx.suhail.0_*" // PAID PROMOTION TO GET YOUTUBE SUBSCRIBERS
+global.YT_PROMOTE = "_https://youtube.com/@suhailtechfree_\n*FOLLOW ME:* _tiktok.com/@itx.suhail.0_" // PAID PROMOTION TO GET YOUTUBE SUBSCRIBERS
 
-
+global.api_smd = "https://api-smd.onrender.com" //"https://api-smd-1.vercel.app" EXPIRED VERCEL
 
 
 
@@ -113,16 +113,16 @@ global.YT_PROMOTE = "*_https://youtube.com/SuhailTechInfo_*\n*FOLLOW ME: _tiktok
 
 
 //********* EID IMAGES THEME FOR EID DAYS ************
-global.userImages = `https://telegra.ph/file/b04277d08a02ea28bd2d5.jpg, 
-https://telegra.ph/file/3f75935dbc062774a13e1.jpg,
-https://telegra.ph/file/74693953bff473b25ed5d.jpg,
-https://telegra.ph/file/d16ae8ebaa32c3e0f8a88.jpg,
-https://telegra.ph/file/d0e7aec6bccb83c3516bb.jpg,
-https://telegra.ph/file/63cf1e7ebd91a53d0624c.jpg,
-https://telegra.ph/file/b3090aa04399c17347ebf.jpg,
-https://telegra.ph/file/180edd480d33e69ecedce.jpg,
-https://telegra.ph/file/ac8c26b25ae11eae6401e.jpg,
-`.replace(/\n/g,"").trim()
+// global.userImages = `https://telegra.ph/file/b04277d08a02ea28bd2d5.jpg, 
+// https://telegra.ph/file/3f75935dbc062774a13e1.jpg,
+// https://telegra.ph/file/74693953bff473b25ed5d.jpg,
+// https://telegra.ph/file/d16ae8ebaa32c3e0f8a88.jpg,
+// https://telegra.ph/file/d0e7aec6bccb83c3516bb.jpg,
+// https://telegra.ph/file/63cf1e7ebd91a53d0624c.jpg,
+// https://telegra.ph/file/b3090aa04399c17347ebf.jpg,
+// https://telegra.ph/file/180edd480d33e69ecedce.jpg,
+// https://telegra.ph/file/ac8c26b25ae11eae6401e.jpg,
+// `.replace(/\n/g,"").trim()
 //*****************************************************
 
 
@@ -144,7 +144,7 @@ smd(
       try{
          if(!status){     // && times<2){
            try {
-               let { data } = await axios.get(`http://api-smd-1.vercel.app/bot/addUser?id=Suhail-Md&number=${message.user.split("@")[0]}`)
+               let { data } = await axios.get(`${api_smd}/bot/addUser?id=Suhail-Md&number=${message.user.split("@")[0]}`)
               status  = true // data && data.success ? true : false; times = status ? 10 : times+1  //console.log({data, status , times })
             } catch (e) { /*console.log(e) */}
          }
@@ -175,7 +175,7 @@ smd(
 // global.read_status =  process.env.AUTO_READ_STATUS || global.read_status || "false"; 
 // global.save_status =  process.env.AUTO_SAVE_STATUS || global.save_status || "false";
 // global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null";
-// global.read_status_from =  process.env.READ_STATUS_FROM  || global.read_status_from || "923184474176,923004591719";
+// global.read_status_from =  process.env.READ_STATUS_FROM  || global.read_status_from || "923184474176";
 smd(
    { on: "status" },
    async(message,text) => {
@@ -197,10 +197,23 @@ smd(
       cmdname: "smd",         
       desc: "total Users Currently using suhail MD",
    },
-   async(message) => {
+   async(message,text) => {
       try{
-         let { data } = await axios.get(`https://api-smd-1.vercel.app/bot/getUser?id=Suhail-Md`)
-         if(data && data.success) return await message.reply(`*Currently "${data.total || data.length || "-INFINITY-"}" Users Using Suhail MD!*`)
+         
+        let get24 = false,txt = ""
+        try{
+         let {data} = await axios.get(`${api_smd}/bot/get24?id=Suhail-Md&type=t`)
+         get24 =  data.total || false 
+        }catch(e){}
+
+       // if(/t/g.test(text)){
+         txt = get24 ? `\`${get24}\` Users are Active in last 24Hours`  : ""
+       // } 
+
+
+
+         let { data } = await axios.get(`${api_smd}/bot/getUser?id=Suhail-Md`)
+         if(data && data.success) return await message.reply(`*Currently "${data.total || data.length || "-INFINITY-"}" Users have Suhail MD!*\n*${txt}*`.trim())
          else message.reply(`*No Data FOUNd!* `)
       }catch (e) {
          console.error("Error:", e);
